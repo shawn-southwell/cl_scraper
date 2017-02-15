5dff882c14a5f8fe7dcf2a63ad4ccd96d14ec883
shawn-southwell <shawn.southwel@gmail.com>
2017-02-15 02:41:18 -0800

    working craigslist crawler

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const rp = require('request-promise');
+const cheerio  = require('cheerio');
+const Promise = require('bluebird');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+	terminal: false
+});
+
+const data = [];
+
+rl.question('WHAT YOU NEED \n', (answer) => {
+
+	const query = answer;
+	rl.question('How many pages? \n', (int) => {
+
+			const URL = `https://losangeles.craigslist.org/search/sss?query=${query}&sort=rel&s=`;
+			const requests = [];
+			let queryNum = (Number(int) * 100) - 100;
+			console.log('lol math', queryNum);
+			let count = 0;
+
+			while(count < queryNum + 1){
+				requests.push(rp(URL+count));
+				count += 100;
+			}
+
+			Promise.each(requests, (request) => {
+					const $ = cheerio.load(request);
+					const totalCount = $('span.totalcount').html();
+					$('.rows').children().each((i,row) => {
+						const title = $(row).find('.result-title').text();
+						const price = $(row).find('.result-price').html() || 'no price avail';
+						const href  = $(row).find('.result-info').find('a').attr('href');
+						const link = `http://losangeles.craigslist.org/${href}`;
+						data.push({ title, price, link });
+					});
+			})
+				.then(() => console.log(data))
+				.catch(err => console.error(err));
+	});
+});
+
