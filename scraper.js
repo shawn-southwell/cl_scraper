66e208ee43cbd35e01d584e1e627294e5f0be0dc
shawn-southwell <shawn.southwel@gmail.com>
2017-02-15 21:11:46 -0800

    modularize scraper behavior

---
+++ scraper.js
@@ -0,0 +1,35 @@
+module.exports = function scrape(readline, Promise, rp, cheerio) {
+return new Promise((resolve) => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+  });
+
+  const data = [];
+
+  rl.question('What are you looking for? \n', (answer) => {
+
+    const query = answer;
+    rl.question('How many pages? \n', (int) => {
+
+        const URL = `https://losangeles.craigslist.org/search/sss?query=${query}&sort=rel&s=`;
+        const requests = [...Array(+int).keys()]
+          .map(int => rp(URL + (int * 100)));
+
+         Promise.each(requests, (request) => {
+            const $ = cheerio.load(request);
+            const totalCount = $('span.totalcount').html();
+            $('.rows').children().each((i,row) => {
+              const title = $(row).find('.result-title').text();
+              const price = $(row).find('.result-price').html() || 'no price avail';
+              const href  = $(row).find('.result-info').find('a').attr('href');
+              const link = `http://losangeles.craigslist.org/${href}`;
+              data.push({ title, price, link });
+            });
+        })
+        .then(() => resolve(data));
+    });
+  });
+})
+}
--- server.js
+++ server.js
@@ -4,37 +4,8 @@ const rp = require('request-promise');
 const cheerio  = require('cheerio');
 const Promise = require('bluebird');
 const readline = require('readline');
+const scraper = require('./scraper.js');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-	terminal: false
-});
-
-const data = [];
-
-rl.question('WHAT YOU NEED \n', (answer) => {
-
-	const query = answer;
-	rl.question('How many pages? \n', (int) => {
-
-			const URL = `https://losangeles.craigslist.org/search/sss?query=${query}&sort=rel&s=`;
-			const requests = [...Array(+int).keys()]
-				.map(int => rp(URL + (int * 100)));
-
-			Promise.each(requests, (request) => {
-					const $ = cheerio.load(request);
-					const totalCount = $('span.totalcount').html();
-					$('.rows').children().each((i,row) => {
-						const title = $(row).find('.result-title').text();
-						const price = $(row).find('.result-price').html() || 'no price avail';
-						const href  = $(row).find('.result-info').find('a').attr('href');
-						const link = `http://losangeles.craigslist.org/${href}`;
-						data.push({ title, price, link });
-					});
-			})
-				.then(() => console.log(data))
-				.catch(err => console.error(err));
-	});
-});
+scraper(readline, Promise, rp, cheerio)
+.then((data) => console.log(data));
 
